2a6238bbcd2c311da853ad019f1c0200672da398
Mahen Gunawardena <mahengunawardena@hotmail.com>
2013-07-14 16:58:34 +0000

    Add index include web.js

--- web.js
+++ web.js
@@ -2,8 +2,12 @@ var express = require('express');
 
 var app = express.createServer(express.logger());
 
+var content = fs.readFileSync('index.html');
+
+var buffer = content.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+  response.send(buffer);
 });
 
 var port = process.env.PORT || 5000;
